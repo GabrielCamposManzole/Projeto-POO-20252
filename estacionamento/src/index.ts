839bbd7f4b67719054237ab08deaf533fe86feaa
GabrielCamposManzole <gh987181127@gmail.com>
2025-08-26 21:55:18 -0300

    teste

--- estacionamento/src/index.ts
+++
@@ -1,9 +0,0 @@
-let nome =  "Estacionamento";
-let preco = 5.00;
-let vagas = 10;
-
-console.log(`Bem-vindo ao ${nome}!`);
-console.log(`Preço por hora: R$ ${preco.toFixed(2)}`);
-console.log(`Vagas disponíveis: ${vagas}`);
-console.log("Aproveite seu tempo aqui!");
-
